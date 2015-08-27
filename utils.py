b28cadcb7729064b024e67a339bb8858f2a6dbbe
Nick Irvine <nirvine@bns-g.com>
2015-08-26 17:37:31 -0700

    fix utils to not be limited and to use the last digit

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 
 def mk_test_hex(n_chars, filler='badbeefdeadfeed'):
     assert len(filler) == 15
-    assert n_chars <= 256
+    #assert n_chars <= 256
     return ''.join([
-        (hex(i/16)[2] if i % 16 == 0 else filler[(i%16)-1]) 
+        (hex(i/16)[-1] if i % 16 == 0 else filler[(i%16)-1]) 
             for i in range(n_chars)
     ])
